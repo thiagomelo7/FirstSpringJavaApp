f74fe6071fd630e72a2ca75c31c37d684e6bdba3
thiagomelo7 <thiago.pereira@olxbr.com>
2025-06-12 23:00:33 -0300

    Create Post Hello World with ID

--- src/main/java/com/thiago/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/thiago/first_spring_app/controller/HelloWorldController.java
@@ -1,11 +1,10 @@
 package com.thiago.first_spring_app.controller;
 
 
+import com.thiago.first_spring_app.domain.User;
 import com.thiago.first_spring_app.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -24,5 +23,10 @@ public class HelloWorldController {
     @GetMapping
     public String helloWorld() {
         return helloWorldService.helloWorld("Thiago");
+    }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id,@RequestBody User body) {
+        return "Hello World Post " + body.getName() + id;
     }
 }
